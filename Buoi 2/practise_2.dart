09bfc8b188310185d9aa3bc10ed0b16c8f9f1aac
VtThuong <thuong23042003@gmail.com>
2024-01-31 13:32:05 +0700

    BTVN

--- Buoi 2/practise_2.dart
+++ Buoi 2/practise_2.dart
@@ -17,8 +17,10 @@ void main(List<String> args) {
   /// Lấy data từ array trên và tạo ra 1 String với giá trị sau:
   /// Đây là kết quả buổi học thứ 2 về Dart: DART BASIC (phần 1)
 
-  // ví dụ
   String s1 =
-      "${arr[3].toString().substring(0, 1).toUpperCase()}${arr[3].toString().substring(1, 3)}";
+      "${arr[3].toString().substring(0, 1).toUpperCase()}${arr[3].toString().substring(1, 3)} "
+      "${arr[5]} ${arr[4]} ${arr[9][1]} ${arr[8][true]} ${arr[8][1]} ${arr[9][0]} ${arr[1]} ${arr[9][2]}"
+      " ${arr[11]["flutter"].toString().substring(0, 1).toUpperCase()}${arr[11]["flutter"].toString().substring(1, 4)}"
+      "${arr[8][10.2]} ${arr[8][false].toUpperCase()} ${arr[10]}";
   print(s1);
 }
